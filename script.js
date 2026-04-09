42c40fd4b8edab5c987ac333646bc1034b207a18
juancamposcbxvi-boop <juan.camposcbxvi@gmail.com>
2026-04-08 21:14:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let productos = [
         id: 1, 
         nombre: "Carcasa Antigolpes", 
         precio: 25000, 
-        imagen: "https://i.postimg.cc/VkNBMD5M/carcasa-jpg" 
+        imagen: "https://i.postimg.cc/1tyznY3z/images.jpg" 
     },
     { 
         id: 2, 
